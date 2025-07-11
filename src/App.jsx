9023a1f87b6cc6a4c84935fcd950e9f3286be42f
Raa0404 <piyushrana007.pr@gmail.com>
2025-07-11 15:05:27 +0530

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-3xl">
-      OTS Nivaran Design Pack – Ready for logic integration
-    </div>
-  );
-}
